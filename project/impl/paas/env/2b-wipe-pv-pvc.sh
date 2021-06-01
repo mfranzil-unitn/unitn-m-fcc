6b323ad2a4e807d389b7b0d5a0f8bc85381735e4
Matteo Franzil <matteofranzil@gmail.com>
2021-06-01 18:37:44 +0200

    spero sia la fine

--- project/impl/paas/env/2b-wipe-pv-pvc.sh
+++ project/impl/paas/env/2b-wipe-pv-pvc.sh
@@ -1,1 +1,1 @@
-kubectl delete pv centodiciotto-psql-pv --grace-period=0 --force && kubectl patch pv centodiciotto-psql-pv -p '{"metadata": {"finalizers": null}}'
+kubectl delete pv -n centodiciotto-dev centodiciotto-psql-pv --grace-period=0 --force && kubectl patch pv -n centodiciotto-dev centodiciotto-psql-pv -p '{"metadata": {"finalizers": null}}'
