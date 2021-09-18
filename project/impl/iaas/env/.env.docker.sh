e4f965bc8a67d0f5270ae16b2af249a60136e7de
Matteo Franzil <matteofranzil@gmail.com>
2021-09-18 19:49:35 +0200

    Routing licence and refresh update

--- project/impl/iaas/env/.env.docker.sh
+++ project/impl/iaas/env/.env.docker.sh
@@ -1,1 +1,14 @@
-../../.env.docker.sh
+# Docker address within IAAS for remote pushing with proxy
+export DOCKER_REG_IP=172.24.4.144
+export DOCKER_REG_PORT=8081
+# Credentials
+export DOCKER_REG_USER=fcc21
+export DOCKER_REG_PASSWORD=FcC21Password123
+# Name of images and namespaces
+export NAMESPACE=centodiciotto-dev
+export WS_IMAGE=centodiciotto-ws
+export SQL_IMAGE=centodiciotto-psql
+# Access to SQL server
+export SQL_USER=sqldiciotto
+export SQL_PASSWORD=2bhturifjnbgtru8ei2938euj
+export SQL_DB=postgres
